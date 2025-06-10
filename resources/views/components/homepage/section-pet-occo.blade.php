41f6329e18b8a454a6658e0ab6eccbc1d7eafd67
Ubuntu_Dev_NHT <huutai90909@gmail.com>
2025-06-10 03:53:52 +0000

    feat(homepage): add staggered animation for pet icons and text

--- resources/views/components/homepage/section-pet-occo.blade.php
+++ resources/views/components/homepage/section-pet-occo.blade.php
@@ -5,7 +5,65 @@
     );
 
 @endphp
-<section class="relative w-full bg-white min-h-screen overflow-hidden">
+@php
+    // Mapping thủ công class và tên ảnh, chuẩn Laravel, dễ maintain
+    $iconItems = [
+        ['class' => 'top-[40%] left-0 w-[110px]', 'image' => 'a1.webp'],
+        ['class' => 'bottom-72 right-[28%] w-[95px]', 'image' => 'a2.webp'],
+        ['class' => 'bottom-[42%] right-[12%] w-[60px] rotate-[20deg]', 'image' => 'a3.webp'],
+        ['class' => 'top-[20%] left-[15%] w-[100px] rotate-[30deg]', 'image' => 'a4.webp'],
+        ['class' => 'top-[10%] left-[35%] w-[100px]', 'image' => 'a5.webp'],
+        ['class' => 'bottom-75 right-0 w-[100px]', 'image' => 'a7.webp'],
+        ['class' => 'top-12 right-[8%] w-[140px]', 'image' => 'a8.webp'],
+        ['class' => 'bottom-[45%] right-[20%] w-[150px] rotate-[40deg]', 'image' => 'a9.webp'],
+        ['class' => 'top-2 right-3 w-[140px]', 'image' => 'a11.webp'],
+        ['class' => 'top-[25%] left-[25%] w-[100px]', 'image' => 'a12.webp'],
+        ['class' => 'bottom-[-25px] left-[35%] w-[7%]', 'image' => 'g20.gif'],
+        ['class' => 'top-[20%] right-[50%] w-[140px]', 'image' => 'a16.webp'],
+        ['class' => 'bottom-75 right-[35%] w-[125px] rotate-[-20deg]', 'image' => 'a20.webp'],
+        ['class' => 'top-[20%] right-[40%] w-[140px]', 'image' => 'a23.webp'],
+        ['class' => 'bottom-[-15px] right-160 w-[6%]', 'image' => 'a30.webp'],
+        ['class' => 'top-[30%] right-0 w-[100px]', 'image' => 'a25.webp'],
+        ['class' => 'bottom-[-15px] right-[50%] w-[6%]', 'image' => 'a32.webp'],
+        ['class' => 'bottom-40 left-[28%] w-[110px]', 'image' => 'a27.webp'],
+        ['class' => 'bottom-2 right-1/2 w-[10px]', 'image' => 'a28.webp'],
+        ['class' => 'bottom-[-30px] left-[17%] w-[11%] rotate-[20deg]', 'image' => 'a35.webp'],
+        ['class' => 'bottom-2 right-195 w-[6%]', 'image' => 'a36.webp'],
+        ['class' => 'bottom-35 right-[35%] w-[105px]', 'image' => 'a26.webp'],
+        ['class' => 'top-2 right-[20%] w-[140px]', 'image' => 'a33.webp'],
+        ['class' => 'bottom-42 right-[45%] w-[125px]', 'image' => 'a34.webp'],
+        ['class' => 'bottom-42 left-[10%] w-[110px]', 'image' => 'a29.webp'],
+        ['class' => 'top-[20%] right-[20%] w-[140px]', 'image' => 'a24.webp'],
+        ['class' => 'top-5 right-[40%] w-[160px]', 'image' => 'a37.webp'],
+        ['class' => 'top-[10%] left-[22%] w-[120px]', 'image' => 'a38.webp'],
+        ['class' => 'top-[5%] left-[28%] w-[100px]', 'image' => 'a39.webp'],
+        ['class' => 'top-8 right-[28%] w-[160px]', 'image' => 'g6.gif'],
+        ['class' => 'top-[45%] right-[45%] w-[150px]', 'image' => 'g7.gif'],
+        ['class' => 'bottom-42 left-[35%] w-[85px]', 'image' => 'g8.gif'],
+        ['class' => 'bottom-[-150px] right-0 w-[35%]', 'image' => 'g13.gif'],
+        ['class' => 'bottom-[-20px] left-0 w-[120px] rotate-[-35deg]', 'image' => 'a13.webp'],
+        ['class' => 'bottom-38 left-[20%] w-[95px]', 'image' => 'g21.gif'],
+        ['class' => 'bottom-8 left-2/3 w-[10px]', 'image' => 'g22.gif'],
+        ['class' => 'top-[32%] right-[10%] w-[140px]', 'image' => 'g27.gif'],
+        ['class' => 'bottom-36 left-[-10px] w-[130px]', 'image' => 'g28.gif'],
+        ['class' => 'top-[20%] left-[5%] w-[120px]', 'image' => 'g33.gif'],
+        ['class' => 'bottom-96 right-5 w-[180px]', 'image' => 'g39.gif'],
+        ['class' => 'top-[36%] right-[32%] w-[180px]', 'image' => 'g22.gif'],
+    ];
+@endphp
+<section class="relative w-full bg-white min-h-screen overflow-hidden" x-data="{
+    showText: false,
+    showIcons: Array({{ count($iconItems ?? []) }}).fill(false),
+    async init() {
+        this.showText = true;
+        // Đợi chữ hiện xong (400ms), rồi lần lượt hiện từng icon
+        await new Promise(r => setTimeout(r, 400));
+        for (let i = 0; i < this.showIcons.length; i++) {
+            this.showIcons[i] = true;
+            await new Promise(r => setTimeout(r, 120)); // delay từng icon
+        }
+    }
+}" x-init="init()">
     <!--
         Icon phủ quanh
     
@@ -13,56 +71,12 @@
         - ảnh được lưu trong public/occo/home/ga_pet
         - thứ tự ảnh trong folder tương ứng với class code bên dưới
     -->
-    @php
-        // Mapping thủ công class và tên ảnh, chuẩn Laravel, dễ maintain
-        $iconItems = [
-            ['class' => 'top-[40%] left-0 w-[110px]', 'image' => 'a1.webp'],
-            ['class' => 'bottom-72 right-[28%] w-[95px]', 'image' => 'a2.webp'],
-            ['class' => 'bottom-[42%] right-[12%] w-[60px] rotate-[20deg]', 'image' => 'a3.webp'],
-            ['class' => 'top-[20%] left-[15%] w-[100px] rotate-[30deg]', 'image' => 'a4.webp'],
-            ['class' => 'top-[10%] left-[35%] w-[100px]', 'image' => 'a5.webp'],
-            ['class' => 'bottom-75 right-0 w-[100px]', 'image' => 'a7.webp'],
-            ['class' => 'top-12 right-[8%] w-[140px]', 'image' => 'a8.webp'],
-            ['class' => 'bottom-[45%] right-[20%] w-[150px] rotate-[40deg]', 'image' => 'a9.webp'],
-            ['class' => 'top-2 right-3 w-[140px]', 'image' => 'a11.webp'],
-            ['class' => 'top-[25%] left-[25%] w-[100px]', 'image' => 'a12.webp'],
-            ['class' => 'bottom-[-25px] left-[35%] w-[7%]', 'image' => 'g20.gif'],
-            ['class' => 'top-[20%] right-[50%] w-[140px]', 'image' => 'a16.webp'],
-            ['class' => 'bottom-75 right-[35%] w-[125px] rotate-[-20deg]', 'image' => 'a20.webp'],
-            ['class' => 'top-[20%] right-[40%] w-[140px]', 'image' => 'a23.webp'],
-            ['class' => 'bottom-[-15px] right-160 w-[6%]', 'image' => 'a30.webp'],
-            ['class' => 'top-[30%] right-0 w-[100px]', 'image' => 'a25.webp'],
-            ['class' => 'bottom-[-15px] right-[50%] w-[6%]', 'image' => 'a32.webp'],
-            ['class' => 'bottom-40 left-[28%] w-[110px]', 'image' => 'a27.webp'],
-            ['class' => 'bottom-2 right-1/2 w-[10px]', 'image' => 'a28.webp'],
-            ['class' => 'bottom-[-30px] left-[17%] w-[11%] rotate-[20deg]', 'image' => 'a35.webp'],
-            ['class' => 'bottom-2 right-195 w-[6%]', 'image' => 'a36.webp'],
-            ['class' => 'bottom-35 right-[35%] w-[105px]', 'image' => 'a26.webp'],
-            ['class' => 'top-2 right-[20%] w-[140px]', 'image' => 'a33.webp'],
-            ['class' => 'bottom-42 right-[45%] w-[125px]', 'image' => 'a34.webp'],
-            ['class' => 'bottom-42 left-[10%] w-[110px]', 'image' => 'a29.webp'],
-            ['class' => 'top-[20%] right-[20%] w-[140px]', 'image' => 'a24.webp'],
-            ['class' => 'top-5 right-[40%] w-[160px]', 'image' => 'a37.webp'],
-            ['class' => 'top-[10%] left-[22%] w-[120px]', 'image' => 'a38.webp'],
-            ['class' => 'top-[5%] left-[28%] w-[100px]', 'image' => 'a39.webp'],
-            ['class' => 'top-8 right-[28%] w-[160px]', 'image' => 'g6.gif'],
-            ['class' => 'top-[45%] right-[45%] w-[150px]', 'image' => 'g7.gif'],
-            ['class' => 'bottom-42 left-[35%] w-[85px]', 'image' => 'g8.gif'],
-            ['class' => 'bottom-[-150px] right-0 w-[35%]', 'image' => 'g13.gif'],
-            ['class' => 'bottom-[-20px] left-0 w-[120px] rotate-[-35deg]', 'image' => 'a13.webp'],
-            ['class' => 'bottom-38 left-[20%] w-[95px]', 'image' => 'g21.gif'],
-            ['class' => 'bottom-8 left-2/3 w-[10px]', 'image' => 'g22.gif'],
-            ['class' => 'top-[32%] right-[10%] w-[140px]', 'image' => 'g27.gif'],
-            ['class' => 'bottom-36 left-[-10px] w-[130px]', 'image' => 'g28.gif'],
-            ['class' => 'top-[20%] left-[5%] w-[120px]', 'image' => 'g33.gif'],
-            ['class' => 'bottom-96 right-5 w-[180px]', 'image' => 'g39.gif'],
-            ['class' => 'top-[36%] right-[32%] w-[180px]', 'image' => 'g22.gif'],
-        ];
-    @endphp
-
-    {{-- Render ảnh với class tương ứng, giữ nguyên layout UI --}}
-    @foreach ($iconItems as $item)
-        <img src="{{ asset('occo/home/ga_pet/' . $item['image']) }}" class="absolute {{ $item['class'] }}" alt="pet icon">
+    @foreach ($iconItems as $i => $item)
+        <img src="{{ asset('occo/home/ga_pet/' . $item['image']) }}"
+            class="absolute {{ $item['class'] }} transition-all duration-500 ease-out" alt="pet icon"
+            x-show="showIcons[{{ $i }}]" x-transition:enter="opacity-0 scale-90 translate-y-4"
+            x-transition:enter-start="opacity-0 scale-90 translate-y-4"
+            x-transition:enter-end="opacity-100 scale-100 translate-y-0" style="display: none;">
     @endforeach
     <div class="absolute inset-0 pointer-events-none select-none">
         @foreach ($icons as $i => $icon)
@@ -73,7 +87,10 @@
     </div>
 
     <!-- Text block bên trái -->
-    <div class="relative min-h-screen flex flex-col justify-center z-10 max-w-3xl pl-6 md:pl-24 lg:pl-36 pr-4 md:pr-0">
+    <div class="relative min-h-screen flex flex-col justify-center z-10 max-w-3xl pl-6 md:pl-24 lg:pl-36 pr-4 md:pr-0"
+        x-show="showText" x-transition:enter="opacity-0 translate-y-8"
+        x-transition:enter-start="opacity-0 translate-y-8" x-transition:enter-end="opacity-100 translate-y-0"
+        style="display: none;">
         <div class="relative w-full flex flex-col items-start">
             <div class="relative z-10 px-6 py-8">
                 <h2 class="text-2xl sm:text-3xl md:text-5xl font-extrabold leading-tight mb-4">
