cd1eb21542d592d8c7a977a24d0bdc595fc522ba
Ubuntu_Dev_NHT <huutai90909@gmail.com>
2025-06-10 09:45:16 +0000

    feat(homepage): uncomment and adjust homepage sections and styling

--- resources/views/components/layouts/assets.blade.php
+++ resources/views/components/layouts/assets.blade.php
@@ -1,2 +1,2 @@
 <script src='{{ asset('build/assets/app-T1DpEqax.js') }}'></script>
-<link href='{{ asset('build/assets/app-DrmPP0Qu.css') }}' rel='stylesheet'>
+<link href='{{ asset('build/assets/app-D41YIW38.css') }}' rel='stylesheet'>
