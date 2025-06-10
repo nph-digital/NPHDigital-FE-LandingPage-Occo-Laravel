2f63c8356bd99a0495e2daf5d5647ef8565709bf
Ubuntu_Dev_NHT <huutai90909@gmail.com>
2025-06-10 04:31:00 +0000

    chore: update asset file hashes in manifest and blade template

--- resources/views/components/layouts/assets.blade.php
+++ resources/views/components/layouts/assets.blade.php
@@ -1,2 +1,2 @@
 <script src='{{ asset('build/assets/app-T1DpEqax.js') }}'></script>
-<link href='{{ asset('build/assets/app-FVMXFrz6.css') }}' rel='stylesheet'>
+<link href='{{ asset('build/assets/app-C7Oj3PPu.css') }}' rel='stylesheet'>
