ecf3fdd4a9a3385a9eff04758c3c08501e3546ff
Ubuntu_Dev_NHT <huutai90909@gmail.com>
2025-06-12 02:06:03 +0000

    style: update asset references and adjust flex direction

--- resources/views/components/homepage/section-pet-occo.blade.php
+++ resources/views/components/homepage/section-pet-occo.blade.php
@@ -83,7 +83,7 @@
 
     {{-- SLIDE ICON DƯỚI --}}
     <div class="w-full overflow-none mt-6">
-        <div class="flex gap-8">
+        <div class="flex flex-row-reverse gap-8">
             @foreach ($iconItemsBottom as $i => $item)
                 <img src="{{ asset('occo/home/ga_pet/' . $item) }}" alt="icon"
                     class="occo-icon-bot w-[100px] h-auto object-contain" x-show="showText">
--- resources/views/components/layouts/assets.blade.php
+++ resources/views/components/layouts/assets.blade.php
@@ -1,2 +1,2 @@
 <script src='{{ asset('build/assets/app-CZeSCoS2.js') }}'></script>
-<link href='{{ asset('build/assets/app-Cmtv4LhD.css') }}' rel='stylesheet'>
+<link href='{{ asset('build/assets/app-DlK5XKU3.css') }}' rel='stylesheet'>
